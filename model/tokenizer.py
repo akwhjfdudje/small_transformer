e4f9368a0661a1c7ae1b080c7a021d45122fe2b5
akwhjfdudje <149972547+akwhjfdudje@users.noreply.github.com>
2025-11-05 14:28:22 -0700

    added new tokenizer to use half words

--- model/tokenizer.py
+++ model/tokenizer.py
@@ -14,15 +14,19 @@ class SimpleTokenizer:
     def build_vocab(self, texts, min_freq=1):
         counts = {}
         for text in texts:
-            for word in re.findall(r"\w+|[^\w\s]", text):
-                counts[word] = counts.get(word, 0) + 1
+            for full_word in re.findall(r"\w+|[^\w\s]", text):
+                for word in [full_word[0:len(full_word)//2], full_word[len(full_word)//2:]]:
+                    counts[word] = counts.get(word, 0) + 1
         for word, freq in counts.items():
             if freq >= min_freq and word not in self.vocab:
                 self.vocab[word] = len(self.vocab)
         self.inv_vocab = {v: k for k, v in self.vocab.items()}
 
     def encode(self, text):
-        words = re.findall(r"\w+|[^\w\s]", text)
+        full_words = re.findall(r"\w+|[^\w\s]", text)
+        words = []
+        for full_word in full_words:
+            words += [full_word[0:len(full_word)//2], full_word[len(full_word)//2:]]
         return [self.vocab.get(w, self.vocab["<unk>"]) for w in words]
 
     def decode(self, ids):
